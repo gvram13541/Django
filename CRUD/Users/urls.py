726012b84825831f6d285be7731f958a0c9d99fb
gvram13541 <guna955reddy@gmail.com>
2024-12-20 16:55:39 +0530

    Class Based Views

--- CRUD/Users/urls.py
+++ CRUD/Users/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from Users.views import UsersListView, UserDetailsView
 
 urlpatterns = [
     path('users/', UsersListView.as_view()),
     path('user_details/<int:pk>', UserDetailsView.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
